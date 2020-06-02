f1adb81a9f55885fcc569e1f9f4592fd110b141e
杨益 <yangyi.cn.gz@gmail.com>
2020-06-02 15:09:26 +0800

    feat(cache): 缓存数据压缩

--- cache.go
+++ cache.go
@@ -1,6 +1,10 @@
 package entity
 
 import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io/ioutil"
 	"time"
 
 	jsoniter "github.com/json-iterator/go"
@@ -27,6 +31,7 @@ type CacheOption struct {
 	Cacher     Cacher
 	Key        string
 	Expiration time.Duration
+	Compress   bool
 	// 某些由其它地方构造的缓存，其中存在字段内容进入缓存前先被json encode过
 	// 这些字段缓存结果需要被decode两次才能使用
 	RecursiveDecode []string
@@ -43,6 +48,20 @@ func loadCache(ent Cacheable) (bool, error) {
 		return false, errors.WithStack(err)
 	} else if len(data) == 0 {
 		return false, nil
+	}
+
+	if opt.Compress {
+		zr, err := gzip.NewReader(bytes.NewReader(data))
+		if err != nil {
+			return false, fmt.Errorf("uncompress cache data, %w", err)
+		}
+		defer zr.Close()
+
+		v, err := ioutil.ReadAll(zr)
+		if err != nil {
+			return false, fmt.Errorf("uncompress cache data, %w", err)
+		}
+		data = v
 	}
 
 	if len(opt.RecursiveDecode) > 0 {
@@ -73,6 +92,18 @@ func SaveCache(ent Cacheable) error {
 	opt, err := getCacheOption(ent)
 	if err != nil {
 		return err
+	}
+
+	if opt.Compress {
+		var zdata bytes.Buffer
+		zw := gzip.NewWriter(&zdata)
+		if _, err := zw.Write(data); err != nil {
+			return fmt.Errorf("compress cache data, %w", err)
+		}
+		if err := zw.Close(); err != nil {
+			return fmt.Errorf("comporess cache data, %w", err)
+		}
+		data = zdata.Bytes()
 	}
 
 	return errors.Wrap(opt.Cacher.Put(opt.Key, data, opt.Expiration), "save entity cache")
